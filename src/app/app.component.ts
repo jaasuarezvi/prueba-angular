77e8c412dbecd450ec70ddaa2a5097fd160bd54c
Jairo Suarez <andres4005@gmail.com>
2022-04-11 08:46:08 -0500

    request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'prueba-angular';
+  title = 'SoyYO prueba';
+  URI ="https://awovcw7p76.execute-api.us-east-1.amazonaws.com/dev/entity/v2.1/entities/1"
+  data: any[] = [];
+
+
+  constructor(
+    private http:HttpClient
+  ){}
+
+  ngOnInit(){
+    this.http.get(this.URI)
+    .subscribe((data: any)=> {
+      this.data = data;
+
+    })
+  }
 }
